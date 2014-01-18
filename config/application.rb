28e07fa447951b498707d3df4026dd497fe7345f
Rafael Polo <rafael.polo@gmail.com>
2014-01-18 01:46:35 +0100

    require 'colored' on minc lib

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'colored'
 
 Bundler.require(:default, Rails.env)
 
--- lib/minc.rb
+++ lib/minc.rb
@@ -1,4 +1,5 @@
 require "net/http"
+require 'colored'
 
 class Minc
 
