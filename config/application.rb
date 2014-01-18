efb04ff2724154dad76bfe79031a3cfbf936824b
Rafael Polo <rafael.polo@gmail.com>
2014-01-18 01:41:59 +0100

    set time_zone and coloured gem

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'colored'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module Mostre
@@ -14,7 +13,7 @@ module Mostre
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Sao Paulo'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
