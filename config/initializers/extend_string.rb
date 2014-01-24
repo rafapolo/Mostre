8589bd010285f146658f53e862441e632e58a738
Rafael Polo <rafael.polo@gmail.com>
2014-01-24 14:55:14 +0100

    fix urlization

--- config/initializers/extend_string.rb
+++ config/initializers/extend_string.rb
@@ -3,10 +3,15 @@ class String
     def clean_extra_spaces
         self.encode.strip.gsub(/\s+/, ' ')
     end
+
+    def remove_dots
+        self.gsub('.', '')
+    end
  
     def urlize
-        copy = self.dup.downcase.clean_extra_spaces
-        accents = { ['á','à','â','ä','ã','Ã','Ä','Â','À'] => 'a',
+        this = self.dup.downcase.clean_extra_spaces.remove_dots
+
+        accents = { ['á','à','â','ä','ã','Ã','Ä','Â','À','Á'] => 'a',
                     ['é','è','ê','ë','Ë','É','È','Ê','&'] => 'e',
                     ['í','ì','î','ï','I','Î','Ì'] => 'i',
                     ['ó','ò','ô','ö','õ','Õ','Ö','Ô','Ò'] => 'o',
@@ -14,15 +19,15 @@ class String
                     ['ß'] => 'ss',
                     [' ', '.', '–', '_'] => '-',
                     ['ç'] => 'c',
-                    ['ú','ù','û','ü','U','Û','Ù'] => 'u',
+                    ['ú','ù','û','ü','U','Û','Ù', 'Ú'] => 'u',
                     ['!',',',':','?',';','(',')','/', '\'', '"'] => ''
         }
         accents.each do |ac,rep|
           ac.each do |s|
-            copy.gsub!(s, rep)
+            this.gsub!(s, rep)
           end
         end
-        copy.gsub(/[-]+/, '-')
+        this.gsub(/[-]+/, '-')
     end
 
    # PeDrO DA CosTa => Pedro da Costa
