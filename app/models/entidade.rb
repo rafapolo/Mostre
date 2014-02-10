1125b5189f77ab1b800f4db4de6ad3b9652c2c4d
Rafael Polo <rafael.polo@gmail.com>
2014-02-10 20:29:51 +0100

    Entidade têm cidade e estado

--- app/models/entidade.rb
+++ app/models/entidade.rb
@@ -1,7 +1,10 @@
 class Entidade < ActiveRecord::Base
-	has_many :projetos
-	has_many :incentivos
+	
 	belongs_to :estado
+	belongs_to :cidade
+
+	has_many :projetos
+	has_many :incentivos	
 
 	scope :patrocinadores, -> {where(patrocinador: 1)}
 	scope :proponentes, -> {where(proponente: 1)}
@@ -11,31 +14,32 @@ class Entidade < ActiveRecord::Base
 	before_save :set_meta_attrs
 	# deduz meta-informações. é proponente? empresa? soma apoios, etc.
 	def set_meta_attrs				
-		estado = Estado.find_by_nome(self.uf)
-		self.estado_id = estado ? estado.id : 0
-
 		self.empresa = self.cnpjcpf.size==14
 		self.proponente = self.projetos.count>0
-		self.patrocinador = self.incentivos.count>0
-		
-		if self.proponente
-			self.projetos_count = self.projetos.count
-			self.projetos_sum = self.projetos.map(&:apoiado).sum.to_f # &:solicitado para FNC ?
-			self.projetos_liberados = Projeto.where(entidade_id: self.id).where('liberado_at is not null').count
-		end
+		self.patrocinador = self.incentivos.count>0		
 
-		if self.patrocinador
-			self.incentivos_count = self.incentivos.count
-			self.incentivos_sum = self.incentivos.map(&:valor).sum.to_f
-		end
+		estado = Estado.find_by_nome(self.uf)
+		self.estado_id = estado ? estado.id : 0
+		
+		self.projetos_count = self.projetos.count
+		self.projetos_sum = self.projetos.map(&:apoiado).sum.to_f # &:solicitado para FNC ?
+		self.projetos_liberados = Projeto.where(entidade_id: self.id).where('liberado_at is not null').count
+
+		self.incentivos_count = self.incentivos.count
+		self.incentivos_sum = self.incentivos.map(&:valor).sum.to_f		
+		self.last_incentivo = self.incentivos.sort_by{:last_recibo_at}.first.last_recibo_at if self.patrocinador
 		
 		self.logradouro = self.logradouro.clean_extra_spaces
 		self.nome = self.nome.normalize if self.nome && !self.empresa
-		self.urlized = self.nome.urlize
+		self.urlized = self.nome.urlize		
 	end
 
 	def to_param		
-		self.urlized
+		"#{self.id}-#{self.urlized}"
+	end
+
+	def similares
+		Entidade.where("nome LIKE ?", "%#{self.nome}%") - [self]
 	end
 	
 end
