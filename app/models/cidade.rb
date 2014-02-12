23083caa0f79f172e053257f9d54f6e84faa0f06
Rafael Polo <rafael.polo@gmail.com>
2014-02-12 15:29:21 +0100

    Cidades geocoded only during development

--- app/models/cidade.rb
+++ app/models/cidade.rb
@@ -2,12 +2,11 @@ class Cidade < ActiveRecord::Base
 	belongs_to :estado
 	has_many :entidades	
 
-
-	geocoded_by :endereco
-	after_validation :geocode
+	# geocoded_by :endereco
+	# after_validation :geocode
 	
-	private
-	def endereco
-		"#{self.nome} - #{self.estado.nome}, Brasil"
-	end
+	# private
+	# def endereco
+	# 	"#{self.nome} - #{self.estado.nome}, Brasil"
+	# end
 end
