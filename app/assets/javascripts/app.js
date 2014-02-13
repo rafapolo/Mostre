b3cf7027841bc78498aac14eff68d59ac81d7460
Rafael Polo <rafael.polo@gmail.com>
2014-02-13 18:41:56 +0100

    + google analytics

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,6 +3,15 @@
 //= require twitter/bootstrap
 //= require code
 
+
+// Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-3761913-6', 'mostre.me');
+ga('send', 'pageview');
+
 var url_params;
 (window.onpopstate = function() {
     var match,
