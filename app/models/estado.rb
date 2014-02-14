f4fbd1b308d845aa18c391021e9693496c73c87b
Rafael Polo <rafael.polo@gmail.com>
2014-02-14 11:39:26 +0100

    remove Estado Desconhecido - XX das listagens

--- app/models/estado.rb
+++ app/models/estado.rb
@@ -3,6 +3,10 @@ class Estado < ActiveRecord::Base
 	has_many :entidades, through: :projetos
 	has_many :cidades
 
+	def all
+		self.all - [Estado.find_by_sigla('XX')] # remove Estado Desconhecido
+	end
+
 	def urlized
 		sigla.downcase
 	end
