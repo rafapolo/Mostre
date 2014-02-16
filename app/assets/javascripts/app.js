2d01ee2364d1747f06c44c61861d310c3fac545a
Rafael Polo <rafael.polo@gmail.com>
2014-02-16 17:33:38 +0100

    google analytics in its own file only in Production env

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,19 +3,10 @@
 //= require twitter/bootstrap
 //= require code
 
-
-// Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-3761913-6', 'mostre.me');
-ga('send', 'pageview');
-
 var url_params;
 (window.onpopstate = function() {
     var match,
-        pl     = /\+/g, // Regex for replacing addition symbol with a space
+        pl     = /\+/g,
         search = /([^&=]+)=?([^&]*)/g,
         decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
         query  = window.location.search.substring(1);
@@ -39,5 +30,3 @@ $.fn.enterKey = function (fnc) {
 function get_param(name) {
     return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 }
-
-$.fn.carousel.defaults = { interval: false }
