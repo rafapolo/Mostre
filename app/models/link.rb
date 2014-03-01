9cc9137321dc36ccc51a1a01acf8437df55dadf1
Rafael Polo <rafael.polo@gmail.com>
2014-03-01 18:16:00 +0100

    remove validates_atalho from link

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,15 +3,13 @@ class Link < ActiveRecord::Base
   has_many :clicks, :dependent => :destroy
 
   validates_length_of :titulo, :in=>5..45, :message=>"deve ter entre 5 e 45 caracteres"
-  validates_length_of :atalho, :in=>5..45, :message=>"gerado deve ter entre 5 e 45 caracteres"
   validates_length_of :para, :minimum=>15, :too_short=>"não pode ter menos de 15 caracteres"
   validates_format_of :para, :message=>"deve ter o formato de uma URL", :with => URI::regexp(%w(http https))
   validates_uniqueness_of :atalho, :message=>"com esse título já foi cadastrado, escolha outro"
 
   before_create :urlize
-  private
   def urlize
-    self.atalho = titulo.urlize
+    self.atalho = self.titulo.urlize
   end
 
 end
