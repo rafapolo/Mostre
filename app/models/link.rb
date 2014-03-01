065d4a8fed049279c71437209002779ca42ad7dc
Rafael Polo <rafael.polo@gmail.com>
2014-03-01 17:47:20 +0100

    fix link "urlization" before save

--- app/models/link.rb
+++ app/models/link.rb
@@ -7,12 +7,10 @@ class Link < ActiveRecord::Base
   validates_length_of :para, :minimum=>15, :too_short=>"não pode ter menos de 15 caracteres"
   validates_format_of :para, :message=>"deve ter o formato de uma URL", :with => URI::regexp(%w(http https))
   validates_uniqueness_of :atalho, :message=>"com esse título já foi cadastrado, escolha outro"
-  before_validation :urlize, :only => [:titulo]
 
+  before_create :urlize
   private
-
   def urlize
-    self.titulo.strip!
     self.atalho = titulo.urlize
   end
 
