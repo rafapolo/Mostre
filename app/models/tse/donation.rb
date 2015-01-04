47485568c3d4b14c3947dd6c3aa4f278ef0dce96
Rafael Polo <rafael.polo@gmail.com>
2015-01-04 12:32:23 +0100

    remove last TSE files

--- app/models/tse/donation.rb
+++
@@ -1,9 +0,0 @@
-class TSE::Donation < ActiveRecord::Base
-  self.table_name = 'tse_donations'
-
-  # Doador -> valor -> Receptor
-  belongs_to :doador, class_name: 'TSE::Entidade', foreign_key: 'doador_id'
-  belongs_to :receptor, class_name: 'TSE::Entidade', foreign_key: 'receptor_id'
-  # Entidade -> (candidato, comite, partido)
-
-end
